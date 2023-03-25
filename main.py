33dacb25f06b45403d6d2362481b26641668b0a2
SS141002 <ssharma141002@gmail.com>
2023-03-25 15:20:26 +0530

    first

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+
+class Arithmetic(Screen):
+    pass
+
+
+file = Builder.load_file("design.kv")
+
+scrmgr = ScreenManager()
+scrmgr.add_widget(Arithmetic(name="scr_ari"))
+
+
+class MyApp(App):
+    def build(self):
+        self.title = "Calculator"
+        # self.icon = "ss.ico"
+        return scrmgr
+
+
+if __name__ == "__main__":
+    MyApp().run()
