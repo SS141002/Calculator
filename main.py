72c75fada4838134177dcebf3f6dd8a1b37a0e2f
SS141002 <ssharma141002@gmail.com>
2023-03-25 19:39:26 +0530

    first

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ Window.custom_titlebar = True
 # Window.borderless = True
 
 
+class Calc:
+    def __init__(self):
+        self.text = ""
+        self.infix = []
+        self.infix_element = 0
+
+    def insert_into_infix(self, index, number, variable, value):
+        self.infix[index] = [number, variable, value]
+
+    def text_to_infix(self):
+        num = 0
+        for i in self.text:
+            if i.isdigit():
+                num += (num * 10) + int(i)
+            else:
+                self.insert_into_infix(self.infix_element, num, '#', '#')
+                num = 0
+
+
 class Arithmetic(Screen):
     pass
 
@@ -23,7 +42,7 @@ scrmgr.add_widget(Arithmetic(name="scr_ari"))
 
 class MyApp(App):
     def build(self):
-        self.title = "Calculator"
+        # self.title = "Calculator"
         # self.icon = "ss.ico"
         return scrmgr
 
